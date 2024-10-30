8bcc8dafc84f59d828735f76a3b1f41445a1884e
hetsaraiya <hetsaraiya06@gmail.com>
2024-10-31 01:04:54 +0530

    Update database connection to use PostgreSQL

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./data.db"
+DATABASE_URL = "postgresql+asyncpg://postgres.hsgstrgqrxxlefxjzgds:VM6JoGpzkSImeRw2@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
