56bd2496160379c6e8f0a72c8cf250a1887ccd78
hetsaraiya <hetsaraiya06@gmail.com>
2024-10-31 18:10:37 +0530

    Increase max overflow for database connection pool and simplify connection error handling

--- app/database.py
+++ app/database.py
@@ -5,30 +5,21 @@ import logging
 
 DATABASE_URL = "postgresql+asyncpg://postgres.hsgstrgqrxxlefxjzgds:BOMRAgaruFD3BOVU@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
 
-# Create the async engine with `statement_cache_size` set to 0
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
     pool_size=10,
-    max_overflow=10,
-    pool_pre_ping=True,
-    pool_recycle=3600,
-    connect_args={
-        "statement_cache_size": 0,
-        "prepared_statement_cache_size": 0
-    }
+    max_overflow=20,
+    connect_args={"statement_cache_size": 0}
 )
 
-AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False,autocommit=False,autoflush=False)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         try:
             yield session
-        except Exception as e:
-            logging.error(f"Database connection error: {e}")
-            raise
         finally:
             await session.close()
 
