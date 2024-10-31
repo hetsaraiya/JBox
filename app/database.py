c0e30fe4145490daf27ea3089b5a3f9277106da6
hetsaraiya <hetsaraiya06@gmail.com>
2024-10-31 12:09:47 +0530

    Update database connection settings and configure statement cache size

--- app/database.py
+++ app/database.py
@@ -2,8 +2,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres.hsgstrgqrxxlefxjzgds:VM6JoGpzkSImeRw2@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
-engine = create_async_engine(DATABASE_URL, echo=True)
+DATABASE_URL = "postgresql+asyncpg://postgres.hsgstrgqrxxlefxjzgds:BOMRAgaruFD3BOVU@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
+
+# Create the async engine with `statement_cache_size` set to 0
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args={"statement_cache_size": 0}
+)
+
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
@@ -13,3 +20,21 @@ async def get_db():
             yield session
         finally:
             await session.close()
+
+
+
+# # app/database.py
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "sqlite+aiosqlite:///./data.db"
+# engine = create_async_engine(DATABASE_URL, echo=True)
+# AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# Base = declarative_base()
+
+# async def get_db():
+#     async with AsyncSessionLocal() as session:
+#         try:
+#             yield session
+#         finally:
+#             await session.close()
